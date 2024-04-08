0841a8b0c98d4e7587b37bd01d2304c0d4dbbf4c
ShantoieT57 <107485443+ShantoieT57@users.noreply.github.com>
2024-04-08 15:44:28 +0200

    Add files via upload

---
+++ Create Schedule Table Script.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE Schedule (
+    AreaName VARCHAR(500),
+    AreaRegion VARCHAR(500),
+    Day VARCHAR(20),
+    Date DATETIME,
+    Stage INT,
+    [TimeOfLoadshedding] VARCHAR(50),
+    [TimeOFRetreival] DateTime
+)
